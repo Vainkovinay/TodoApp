0ec714e33f02988a0cbd4c24bd2c93543dd6fbc5
Vainkovinay <vinayshrimali03@gmail.com>
2024-03-13 12:37:52 +0530

    Todo-app added

---
+++ App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Todo List</Text>
+        <TextInput style={styles.input}placeholder='Type here' ></TextInput>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container2}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 3,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    flex: 3,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container3: {
+    flex:3,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  input: {
+    height:35,
+    width: 900,
+    borderColor: 'grey',
+    borderWidth: 1.5,
+    textAlign:'center',
+    padding: 7,
+    marginTop: 20, 
+  },
+});
